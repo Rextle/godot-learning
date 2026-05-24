446b7eeecda47c1487744f2431b92e07e654ec00
Rextle <rextlecreations@gmail.com>
2026-05-24 21:16:44 +0200

    adjust 0.1

--- scripts/box.gd
+++ scripts/box.gd
@@ -6,4 +6,11 @@ func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed * delta
+	if Input.is_action_pressed("ui_left"):
+		position.x -= speed * delta
+	if Input.is_action_pressed("ui_up"):
+		position.z -= speed * delta
+	if Input.is_action_pressed("ui_down"):
+		position.z += speed * delta
