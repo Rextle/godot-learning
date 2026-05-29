c1e78360c16784fca827c8b7935a7acf4a4dfb11
Rextle <rextlecreations@gmail.com>
2026-05-29 04:48:48 +0200

    timer

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,10 +16,16 @@ func _physics_process(delta: float) -> void:
 		
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = -500
-	
+	var timer = get_node("/root/World/CountdownTimer")
+	var time_left = ceil(timer.time_left)
+	get_node("/root/World/HUD/TimerLabel").text = "Time: " + str(time_left)
 	move_and_slide()
 	
 func add_score(amount):
 	score += amount
 	var label = get_node("/root/World/HUD/ScoreLabel")
 	label.text = "Score: " + str(score)
+
+
+func _on_countdown_timer_timeout() -> void:
+	get_tree().reload_current_scene()
